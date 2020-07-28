c93014205c4ce59529526071d0474255c70ed76c
Gabriel Cordeiro <henrique.gcordeiro@gmail.com>
2020-07-28 16:24:04 -0300

    Update favicon, carrossel e novas páginas
    Adição do pacote react-slick
    Inclusão das páginas de cadastro de categorias e de cadastro de vídeos
    Alteração na estrutura de arquivos
    Implementação do carrossel na listagem de vídeos
    Alteração do favicon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Fazer a página de erro 404
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      {/* Carrega se não encontrar nenhuma outra rota */}
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>,
   document.getElementById('root')
 );
