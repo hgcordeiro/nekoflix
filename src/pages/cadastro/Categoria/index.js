f2019c0550f4eaf4490fd989436dd366d3b46f33
Gabriel Cordeiro <henrique.gcordeiro@gmail.com>
2020-07-30 21:58:47 -0300

    FormField, Cadastro de Categorias, eslint
    Alteração nos estilos do form e inclusão do eslint

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-  
   const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor: ''
-  }
+    cor: '',
+  };
 
-  const [ categorias, setCategorias ] = useState([]);
-  const [ categoria, setCategoria] = useState({});
+  const [categorias, setCategorias] = useState([]);
+  const [fields, setFields] = useState({});
 
   function setChaveCategoria(key, value) {
-    setCategoria({
-      ...categoria,
-      [key]: value
+    setFields({
+      ...fields,
+      [key]: value,
     });
   }
 
@@ -26,61 +26,63 @@ function CadastroCategoria() {
   }
 
   return (
-      <PageDefault>
-        <h1>Cadastro de Categoria: {categoria.nome}</h1>
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {fields.nome}
+      </h1>
 
-        <form onSubmit={function handleSubmit(event) {
-          // Evita comportamento padrão do form (não recarrega a página)
-          event.preventDefault();
-          setCategorias([
-            ...categorias,
-            categoria
-          ]);
-          setCategoria(valoresIniciais);
-        }}>
-          <FormField 
-            label="Nome da Categoria"
-            name="nome"
-            type="text"
-            value={categoria.nome}
-            onChange={handleChange}
-          />
-          <FormField 
-            label="Descrição"
-            name="descricao"
-            type="textarea"
-            value={categoria.descricao}
-            onChange={handleChange}
-          />
-          <FormField 
-            label="Cor"
-            name="cor"
-            type="color"
-            value={categoria.cor}
-            onChange={handleChange}
-          />
+      <form onSubmit={function handleSubmit(event) {
+        // Evita comportamento padrão do form (não recarrega a página)
+        event.preventDefault();
+        setCategorias([
+          ...categorias,
+          fields,
+        ]);
+        setFields(valoresIniciais);
+      }}
+      >
+        <FormField
+          label="Nome da Categoria"
+          name="nome"
+          type="text"
+          value={fields.nome}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Descrição"
+          name="descricao"
+          type="textarea"
+          value={fields.descricao}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Cor"
+          name="cor"
+          type="color"
+          value={fields.cor}
+          onChange={handleChange}
+        />
 
-          <button>Cadastrar</button>
+        <Button>Cadastrar</Button>
 
-          <ul>
-            {
-              categorias.map((categoria, indice) => {
-                return (
-                  <li key={`${categoria.nome}${indice}`}>
-                    {categoria.nome}
-                  </li>
-                )
-              })
+        <ul>
+          {
+              categorias.map((categoria) => (
+                <li key={categoria.nome}>
+                  {fields.nome}
+                </li>
+              ))
             }
-          </ul>
+        </ul>
 
-        </form>
+      </form>
 
-        <Link to="/">
-          Ir para home
-        </Link>
-      </PageDefault>
-    )
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
